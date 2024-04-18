0494a8b4aed826934cf1a780c607d842667f9e87
student <student@WIN-H7M01J10L01>
2024-04-18 13:57:40 +0900

    7th

--- src/p01/Sample05.java
+++ src/p01/Sample05.java
@@ -1,12 +1,13 @@
 package p01;
 
-public class Sample03 {
+public class Sample05 {
+	//static int a; //メンバ変数
 	
 	public static void main(String[] args) {
-		long a = 2_200_000_000L;
-		double b = 3.14;
-		float c = 3.14f;
-		boolean d = false;
+		long a = 10; //long a = 10L;
+		double b = 3.14F; //double b = 3.14;
+		int c = (int)a;
+		int d = (int)b;
 		
 		System.out.println(a);
 		System.out.println(b);
