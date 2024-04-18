ae589fab54c9a95998b2c27339435f0484cfd3de
student <student@WIN-H7M01J10L01>
2024-04-18 18:01:01 +0900

    実践課題01

---
+++ src/kadai1/CheckInteger4.java
@@ -0,0 +1,21 @@
+package kadai1;
+
+import java.util.Scanner;
+
+public class CheckInteger4 {
+
+	public static void main(String[] args) {
+		//キーボード入力するためのおまじない
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("数字を入力してください：");
+		int data = scan.nextInt();
+		
+		String judge1 = data >= 0 ? "正" : "負";
+		String judge2 = data % 2 == 0 ? "偶数" : "奇数";
+		
+		System.out.println(data + "は" + judge1 + "の" + judge2 + "です");
+
+	}
+
+}
