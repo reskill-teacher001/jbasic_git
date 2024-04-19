6b2d2ce50f2ebeadc14a448d0d54fb82e129f5e3
student <student@WIN-H7M01J10L01>
2024-04-19 17:11:04 +0900

    実践課題JB20とJB21

---
+++ src/kadai2/Introduce2.java
@@ -0,0 +1,35 @@
+package kadai2;
+
+import java.util.Scanner;
+
+public class Introduce2 {
+
+	public static void main(String[] args) {
+		//キーボード入力のおまじない
+		Scanner scan = new Scanner(System.in);
+		
+		String name;
+		String[] hobby = new String[3];
+		
+		System.out.println("情報を登録します。");
+		System.out.print("名前：");
+		name = scan.next();
+		
+		for (int i = 0; i < hobby.length; i++) {
+			System.out.print("趣味" + (i + 1) + "：");
+			hobby[i] = scan.next();
+		}
+		
+		System.out.println("-----------------------------");
+		
+		System.out.println("こんにちは" + name + "です");
+		System.out.println("私の趣味は、");
+		
+		for (String h : hobby) {
+			System.out.println("・" + h);
+		}
+		
+		System.out.println("です");
+	}
+
+}
