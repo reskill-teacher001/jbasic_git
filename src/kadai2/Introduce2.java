76c9701e364eef0ef116ae70a32a8effef322901
student <student@WIN-H7M01J10L01>
2024-04-24 15:35:30 +0900

    追加２

--- src/kadai2/Introduce2.java
+++ src/kadai2/Introduce2.java
@@ -8,12 +8,22 @@ public class Introduce2 {
 		//キーボード入力のおまじない
 		Scanner scan = new Scanner(System.in);
 		
-		String name;
+		//String name;
 		String[] hobby = new String[3];
+		//String[] hobby = {null, null, null};
 		
 		System.out.println("情報を登録します。");
 		System.out.print("名前：");
-		name = scan.next();
+		String name = scan.next();
+		
+//		System.out.print("趣味1：");
+//		hobby[0] = scan.next();
+//		
+//		System.out.print("趣味2：");
+//		hobby[1] = scan.next();
+//		
+//		System.out.print("趣味3：");
+//		hobby[2] = scan.next();
 		
 		for (int i = 0; i < hobby.length; i++) {
 			System.out.print("趣味" + (i + 1) + "：");
@@ -28,6 +38,10 @@ public class Introduce2 {
 		for (String h : hobby) {
 			System.out.println("・" + h);
 		}
+		
+//		for (int i = 0; i < hobby.length; i++) {
+//			System.out.print(hobby[i]);
+//		}
 		
 		System.out.println("です");
 	}
